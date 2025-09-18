5a3edf4118e8a69f96632fbb5c8d041fa5d4f68d
Alex Firilov <alexfirilov@gmail.com>
2025-09-18 14:11:19 +0300

    Auth service register and login routes fully working

--- services/auth-service/index.js
+++ services/auth-service/index.js
@@ -3,6 +3,9 @@ const { body, validationResult } = require('express-validator');
 const { PrismaClient } = require('./generated/prisma');
 const prisma = new PrismaClient();
 const bcrypt = require ('bcrypt');
+const jsonjwt = require('jsonwebtoken');
+
+const jwt_secret = process.env.JWT_SECRET
 
 const app = express();
 
@@ -20,8 +23,7 @@ app.get ('/healthz', (req, res) => {
 
 app.post(
   '/register',
-  body('email').isEmail(),
-  body('password').isLength({ min: 8 }),
+  [ body('email').isEmail(), body('password').isLength({ min: 8 }) ],
   async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
@@ -36,7 +38,69 @@ app.post(
       'password': hashedPassword
       }
     };
-    const newUser = await prisma.user.create(user);
-    return res.status(200).json(newUser);
-  }
-);
+
+    try {
+      
+      const existingUser = await prisma.user.findUnique({
+        where: { 'email': email }
+      });
+
+
+      if (existingUser) {
+        return res.status(409).json({"message": "User already exists"});
+      }
+
+      const newUser = await prisma.user.create(user);
+
+      const payload = {
+          "userId": newUser.id
+      }
+
+      const jwt = jsonjwt.sign(payload,jwt_secret,{ expiresIn: '1h' });
+
+      return res.status(201)
+      .json({ "message": "User created successfully", "userId": newUser.id , "userEmail": newUser.email, "jwt": jwt})
+      }
+    catch(error) {
+      return res.status(500).json({"message": "Oops! Something went wrong, please try agian later"})
+     }
+      })
+
+app.post(
+  '/login',
+  [ body('email').isEmail(),body('password').notEmpty() ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    } 
+    const { email, password } = req.body;
+
+    try {
+      const loggedUser = await prisma.user.findUnique({
+        where: {
+          "email": email
+        }
+      })
+      if (!loggedUser) {
+        return res.status(404).json({"message": "Invalid credentials"});
+      }
+
+      const passwordMatch = await bcrypt.compare(password, loggedUser.password)
+
+      if (!passwordMatch) {
+        return res.status(401).json({"message": "Invalid credentials"});
+      }
+
+      const payload = {
+          "userId": loggedUser.id
+      }
+      const jwt = jsonjwt.sign(payload,jwt_secret,{ expiresIn: '1h' });
+
+      return res.status(200).json({"message": "Logged in successfuly!", "jwt": jwt})
+    }
+  
+    catch(error) {
+      return res.status(500).json({"message": "Oops! Something went wrong, please try again later"})
+    }
+})
