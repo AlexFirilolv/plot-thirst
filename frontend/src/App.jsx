c8b6d2b236573b7189077f0419722a99626fec25
Alex Firilov <alexfirilov@gmail.com>
2025-09-22 02:13:26 +0300

    Working register + login page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -33,6 +33,34 @@ function App() {
       alert("Oops! An error occured, please try again later")
     }
   }
+
+  async function handleLogin(e) {
+    e.preventDefault();
+
+    const bodyData = { email, password };
+    const stringifiedBody = JSON.stringify(bodyData);
+
+    try {
+
+      const response = await fetch(`${backendUrl}/login`,
+        { "method": 'POST', "headers": { 'Content-Type': 'application/json' }, "body": stringifiedBody});
+
+      const data = await response.json();
+
+      if(response.ok) {
+        localStorage.setItem('jwt', data.jwt)
+        alert(data.message);
+      }
+      
+      if(!response.ok) {
+        alert(data.message);
+      }
+
+    } catch(error) {
+      alert("Oops! An error occured, please try again later")
+    }
+  }
+
   return (
     <>
       <form onSubmit={handleRegister}>
@@ -40,6 +68,13 @@ function App() {
         <input type="email" placeholder="Your email" value={email} onChange={(event) => setEmail(event.target.value)}/>
         <input type="password" placeholder="Your password" value={password} onChange={(event) => setPassword(event.target.value)}/>
         <button type="submit">Register</button>
+      </form>
+
+      <form onSubmit={handleLogin}>
+        <h1>Login</h1>
+        <input type="email" placeholder="Your email" value={email} onChange={(event) => setEmail(event.target.value)}/>
+        <input type="password" placeholder="Your password" value={password} onChange={(event) => setPassword(event.target.value)}/>
+        <button type="submit">Login</button>
       </form>
     </>
   )
